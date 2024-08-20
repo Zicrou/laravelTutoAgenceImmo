17d9af824267c5f496c64e8cbd213f01a999f05b
Zicrou <aazseck@gmail.com>
2024-08-20 18:18:51 +0000

    Generated Breeze Auth link

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,43 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+$idRegex = '[0-9]+';
+$slugRegex = '[0-9a-z\-]+';
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[\App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/biens',[\App\Http\Controllers\PropertyController::class, 'index'])->name('property.index');
+Route::get('/biens/{slug}-{property}', [\App\Http\Controllers\PropertyController::class, 'show'])->name('property.show')->where([
+    'property' => $idRegex,
+    'slug' => $slugRegex
+]);
+
+Route::post('/biens/{property}-/contact', [App\Http\Controllers\PropertyController::class,'contact'])->name('property.name')->where([
+    'property' => $idRegex
+]);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+
+//Route::get('/images/{path}', [\App\Http\Controllers\ImagesController::class,'show'])->where('path', '.*');
+
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () use ($idRegex){
+    Route::resource('property', \App\Http\Controllers\Admin\PropertyController::class)->except(['show']);
+    Route::resource('option', \App\Http\Controllers\Admin\OptionController::class)->except(['show']);
+    Route::delete('picture/{picture}', [\App\Http\Controllers\Admin\OptionController::class, 'destroy'])
+    ->name('picture.destroy')
+    ->where([
+        'picture' => $idRegex,
+    ])
+    ->can('delete', 'picture');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    });
 
 require __DIR__.'/auth.php';
