8063ff585159f08b23e45c4d4ea67e47504142ca
Zicrou <aazseck@gmail.com>
2024-09-10 02:14:19 +0000

    Added Api with resources(via resources or collection)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFive();
+        JsonResource::withoutWrapping();
     }
 }
